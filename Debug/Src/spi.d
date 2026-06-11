7021db92ab7a4be465b7f4d68b343c16d5e90964
SREERAJ KRISHNA <107305138+SREERAJSVT@users.noreply.github.com>
2026-06-12 00:34:02 +0530

    Initial commit

---
+++ Debug/Src/spi.d
@@ -0,0 +1,19 @@
+Src/spi.o: ../Src/spi.c \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/Inc/spi.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/core_cm4.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/cmsis_version.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/mpu_armv7.h \
+ D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/Inc/spi.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/core_cm4.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/cmsis_version.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/cmsis_compiler.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/cmsis_gcc.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Include/mpu_armv7.h:
+D:/CodeEmbeded/FinalDriverProjectADXLSTM32/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ Inc/uart.h
@@ -0,0 +1,15 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jun 11, 2026
+ *      Author: sreer
+ */
+
+#ifndef __UART_H__
+#define __UART_H__
+
+#include "stm32f4xx.h"
+
+void uart_init(void);
+
+#endif /*UAR*/
